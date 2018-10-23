6f9231b12f4fc8c5efe10579e44a0345cada4d08
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-10-23 02:22:57 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,2 @@
 # Define the variable board below.
-board = ["postion1","position2","positon3","position4","position5","position6","position7","position8","position9"]
+board = [" "," "," "," "," "," "," ",
